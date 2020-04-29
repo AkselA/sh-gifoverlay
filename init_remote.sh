06de43fc200bdf03e8ec794d9f2df42c47b8bf2d
AkselA <akselahenriksen@gmail.com>
2020-04-29 14:55:18 +0200

    removed .gitignore

---
+++ init_remote.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+Dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $Dir
+
+cat > .gitignore <<IGNORE
+.DS_Store
+init_remote.*
+commmit.*
+IGNORE
+
+git init
+git add .
+git commit -m "First commit"
+
+echo "Enter remote URL, e.g git@github.com:... or https://github.com/..."
+read url
+
+if [ -z "$url" ]
+  then
+    echo "aborting due to missing remote URL"
+    exit 1
+fi
+
+git remote add origin "$url"
+
+git push -u origin master
+
+
